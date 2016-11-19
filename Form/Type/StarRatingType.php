f1f7ce484092ddcb1577f03f61f42906ceeac4a1
Jan Machala <jan.machala@email.cz>
2016-11-19 16:27:13 +0100

    Fix compatibility with symfony 3.0

--- Form/Type/StarRatingType.php
+++ Form/Type/StarRatingType.php
@@ -3,16 +3,17 @@
 namespace Nurikabe\StarRatingBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class StarRatingType extends AbstractType
 {
-    public function getName()
+    public function getBlockPrefix()
     {
         return 'star_rating';
     }
 
     public function getParent()
     {
-        return 'choice';
+        return ChoiceType::class;
     }
 }
